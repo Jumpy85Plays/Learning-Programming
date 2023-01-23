ab9c742c829e862b362c793b08ae9b8804cb8e34
Jumpy85Plays <gabriel42498@gmail.com>
2023-01-23 15:28:50 +0800

    update

---
+++ Python/January Activities/23/multiple input proper.py
@@ -0,0 +1,11 @@
+print("Please enter five grades. One at a time.")
+
+grade1 = input("Grade 1: ")
+grade2 = input("Grade 2: ")
+grade3 = input("Grade 3: ")
+grade4 = input("Grade 4: ")
+grade5 = input("Grade 5: ")
+grade= int(grade1) + int(grade2) + int(grade3) + int(grade4) + int(grade5) 
+print(grade)
+
+
