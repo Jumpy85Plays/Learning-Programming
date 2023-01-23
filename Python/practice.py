83378bd63f68dae140a5ce76f15556d3975784f2
Jumpy85Plays <gabriel42498@gmail.com>
2023-01-23 15:13:46 +0800

    Update

--- Python/practice.py
+++ Python/practice.py
@@ -1,5 +1,7 @@
-character_name = "John Cena"
-character_age = 56
-
-print("In the middle of the night, " + character_name + " " + " appears out of nowhere suprising people while shouting you can't see me.")
-print(character_name +" is currently " + str(character_age) + " " + " years old") 
+#the letter A
+print("        * ")
+print("     *    *")
+print("   *       *")
+print("  ************")
+print(" *            *")
+print("*              *")
