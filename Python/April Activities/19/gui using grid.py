86c53b27850de8b3c2dab32ef2838502ebce9c18
Jumpy85Plays <gabriel42498@gmail.com>
2023-04-19 13:58:17 +0800

    Python Update

--- Python/April Activities/19/gui using grid.py
+++ Python/April Activities/19/gui using grid.py
@@ -7,10 +7,10 @@ for i in range(5):
         frame = tk.Frame(
             master=window,
             relief=tk.RAISED,
-            borderwidth=1
+            borderwidth=1            
         )
         frame.grid(row=i, column=j)
-        label = tk.Label(master=frame, text=f"Row {i+1}\nColumn {j+1}", bg="Blue", fg="White")
+        label = tk.Label(master=frame, text=f"Row {i+1}\nColumn {j+1}", bg="Black", fg="White")
         label.pack()
 
 window.mainloop()
